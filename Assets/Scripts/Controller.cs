7622e6e59aacd8e3e170a038309c526cdceae641
KyleVanHook <141291970+KyleVanHook@users.noreply.github.com>
2025-11-13 19:32:42 -0700

    First Commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class Controller : MonoBehaviour
+{
+    //add stuff when needed
+    public Pawn pawn;
+
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class Pawn : MonoBehaviour
+{
+    //add stuff when needed
+
+
+}
